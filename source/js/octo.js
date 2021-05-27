298a93e6e51555f1d00c5e2c9ab5b78dd954a045
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2021-05-27 20:54:40 +0100

    Cleanup

--- source/js/octo.js
+++ source/js/octo.js
@@ -69,27 +69,6 @@ function addCodeLineNumbers() {
     });
 }
 
-function flashVideoFallback(){
-    var flashplayerlocation = "/assets/jwplayer/player.swf",
-        flashplayerskin = "/assets/jwplayer/glow/glow.xml";
-    $('video').each(function(i, video){
-        video = $(video);
-        if (!Modernizr.video.h264 && swfobject.getFlashPlayerVersion() || window.location.hash.indexOf("flash-test") !== -1){
-            video.children('source[src$=mp4]').first().map(i, function(source){
-                var src = $(source).attr('src'),
-                    id = 'video_'+Math.round(1 + Math.random()*(100000)),
-                    width = video.attr('width'),
-                    height = parseInt(video.attr('height'), 10) + 30;
-                video.after('<div class="flash-video"><div><div id='+id+'>');
-                swfobject.embedSWF(flashplayerlocation, id, width, height + 30, "9.0.0",
-                    { file : src, image : video.attr('poster'), skin : flashplayerskin } ,
-                    { movie : src, wmode : "opaque", allowfullscreen : "true" }
-                );
-            });
-            video.remove();
-        }
-    });
-}
 
 function wrapFlashVideos() {
     $('object').each(function(i, object) {
@@ -112,7 +91,6 @@ function renderDeliciousLinks(items) {
 $('document').ready(function() {
     testFeatures();
     wrapFlashVideos();
-    flashVideoFallback();
     addCodeLineNumbers();
     getNav();
     addSidebarToggler();
